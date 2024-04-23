290e84394cc967f1b1bb9ead35dc0c75b5c39e85
simonestrada22 <166950172+simonestrada22@users.noreply.github.com>
2024-04-23 17:56:26 -0500

    Update node.py
    class Node were updated

--- utils/node.py
+++ utils/node.py
@@ -1,9 +1,9 @@
 # utils/node.py
 
 class Node:
-    def __init__(self, value=None):
-        self.value = value
-        self.prev = None
-        self.next = None
+    def __init__(self):
+        self.value = None
         self.up = None
         self.down = None
+        self.prev = None
+        self.next = None
